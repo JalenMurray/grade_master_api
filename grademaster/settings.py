9ac3067ab0fcc8ad3a5a260acea4aece9ac47ad5
Jalen Murray <jalenmmurray@gmail.com>
2024-01-12 23:17:58 -0500

    Trying to fix static files

--- grademaster/settings.py
+++ grademaster/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'https://grade-master-api.onrender.com/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
